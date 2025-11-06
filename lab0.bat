725b7ce31163f47c42975fe408f19c64652de4f8
fsultana980 <fahimasultanasmrity90@gmail.com>
2025-11-07 01:25:39 +0600

    Implement user authentication flow

---
+++ lab0.bat
@@ -0,0 +1,67 @@
+<mobileinformations>
+<mobileinformation>
+        <id>11</id>
+        <name>Samsung</name>
+        <unit_price>35000</unit_price>
+        <stock>50</stock>
+        <status>1</status>
+</mobileinformation><br>
+<mobileinformation>
+        <id>20</id>
+        <name>HTC</name>
+        <unit_price>65300</unit_price>
+        <stock>35</stock>
+        <status>0</status>
+</mobileinformation><br>
+<mobileinformation>
+        <id>23</id>
+        <name>Nokia</name>
+        <unit_price>25100</unit_price>
+        <stock>100</stock>
+        <status>1</status>
+</mobileinformation>
+</mobileinformations>
+
+
+<customerinformations>
+<customerinformation>
+        <sl_no>23</sl_no>
+        <name>John Carter</name>
+    <admission_date>25-12-2015</admission_date>
+</customerinformation><br>
+<customerinformation>
+        <sl_no>29</sl_no>
+        <name>Bob Marsal</name>
+    <admission_date>13-09-1999</admission_date>
+</customerinformation><br>
+<customerinformation>
+        <sl_no>35</sl_no>
+        <name>Clark Kent</name>
+    <admission_date>17-06-2005</admission_date>
+</customerinformation>
+</customerinformations>
+
+
+<employeeinformations>
+<employeeinformation>
+        <id>1</id>
+        <name>John Doe</name>
+        <address>H#38,R#3, Banani, Dhaka</address>
+        <department>Sales</department>
+        <salary>$20000</salary>
+</employeeinformation><br>
+<employeeinformation>
+        <id>2</id>
+        <name>Mark Zuckerberg</name>
+        <address>Flat# A/1, H#20, R#18, Mirpur, Dhaka</address>
+        <department>Accounts</department>
+        <salary>$12000</salary> 
+</employeeinformation><br>
+<employeeinformation>
+        <id>3</id>
+        <name>Ala voski</name>
+        <address>H#38, R#3, Motijheel, Dhaka</address>
+        <department>Human Resource</department>
+        <salary>$30000</salary>
+</employeeinformation>
+</employeeinformations>
